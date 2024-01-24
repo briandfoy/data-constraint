fd9330656b93d9dc5b6c11f7b07d654bf74c4357
brian d foy <briandfoy@pobox.com>
2024-01-24 15:03:47 -0500

    Update email address

--- lib/Data/Constraint.pm
+++ lib/Data/Constraint.pm
@@ -260,11 +260,11 @@ This source is in Github:
 
 =head1 AUTHOR
 
-brian d foy, C<< <bdfoy@cpan.org> >>
+brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2004-2023, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2004-2023, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the Artistic License 2.0.
